86cfbbe39206c41c3eef11b5e51230b0b41d5592
Sidney Edwards <sdwards1@gmail.com>
2023-08-03 18:27:20 -0500

    front-end-things

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,29 @@
 import Page from "../components/Page";
 
+
+
+
 const headContent = (
   <>
     <title>Affirmations</title>
-    <meta name="description" content="This is the home page of my app." />
+    <meta name="description" content="This is the home page of Affirmations, a habit Tracking Application." />
   </>
 );
 
+
 export default function Home() {
   return (
-    <Page isProtected={false} headContent={headContent}>
+    <Page isProtected={false} headContent={headContent} pageStyles={{
+      backgroundImage: `url(/sign2.jpg)`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    }}>
       <div>Home</div>
+
     </Page>
   );
 }
+
+
+
---
+++ client/src/sections/@dashboard/app/index.js
@@ -0,0 +1,5 @@
+export { default as BigPictureTasks } from './BigPictureTasks';
+export { default as AppNewsUpdate } from './AppNewsUpdate';
+export { default as WhereIsYourTimeSpent } from './WhereIsYourTimeSpent';
+export { default as ProgressTimeLine } from './ProgressTimeLine';
+export { default as HabitsInMotion } from './HabitsInMotion';
