4a6f3554b6bd35a5d10e0dee1db8be9bc9867eb4
Sidney Edwards <sdwards1@gmail.com>
2023-08-05 17:41:30 -0500

    stylinghabits

--- client/src/pages/ManageHabits.jsx
+++ client/src/pages/ManageHabits.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import HabitForm from '../components/HabitForm';
 import HabitList from '../components/HabitList';
 import Page from '../components/Page';
-import { Box, Grid, Typography, Button } from '@mui/material';
+
 import button from '../components/button';
 import { ThemeProvider } from '@mui/material/styles';
-
+import { styled } from '@mui/system';
+import { Grid, Box, Button, Card } from '@mui/material';
 
 const App = () => {
 const [habits, setHabits] = useState([]);
@@ -19,6 +20,25 @@ const deleteHabit = (index) => {
     setHabits(newHabits);
 };
 
+const StyledCard = styled(Card)({
+    padding: '20px',
+    width: '100%',
+    maxWidth: '400px',
+    boxSizing: 'border-box',
+    borderRadius: '15px',
+    backgroundColor: 'rgba(0,0,0,.4)',
+    color: 'white'
+
+});
+
+const Container = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'left',
+    justifyContent: 'left',
+    minHeight: '100vh',
+});
+
 return (
     <Page isProtected={false} pageStyles={{
         backgroundImage: `url(neon-bust.jpg)`,
@@ -26,18 +46,20 @@ return (
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         }}>
-
+<Container>
+<Grid>
 <Box
         sx={{
         display: 'flex',
         flexDirection: { xs: 'column', sm: 'row' },
-        alignItems: 'center',
+        alignItems: 'left',
         justifyContent: 'center',
-        padding: 20
+        padding: 20,
+
         }}
     >
 
-    <div>
+<StyledCard>
     <h1>Habit Tracker</h1>
     <HabitForm addHabit={addHabit} />
     <HabitList habits={habits} deleteHabit={deleteHabit} />
@@ -45,13 +67,16 @@ return (
     <Button
     href='/Dashboard'
     variant='contained'
-    size='large'
+    size='medium'
     style={{ marginRight: 250, marginTop: 300 }} 
+
     > Back to Your Dashboard
     </Button>
     </ThemeProvider>
-    </div>
+    </StyledCard>
     </Box>
+    </Grid>
+    </Container>
     </Page>
 
 );
